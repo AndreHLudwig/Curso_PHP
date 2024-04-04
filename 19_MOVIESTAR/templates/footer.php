f82b01a61baaacdc9a5efa540c52cfbfe4b6d605
AndreHLudwig <andre.udesc@hotmail.com>
2024-04-04 18:02:06 -0300

    forms_front

--- 19_MOVIESTAR/templates/footer.php
+++ 19_MOVIESTAR/templates/footer.php
@@ -25,7 +25,7 @@
                <li><a href="#">Entrar / Registrar</a></li> 
             </ul>
         </div>
-        <p>&copy; 2024 EMPDS</p>
+        <p>&copy; 2024 EMPDS- André Ludwig </p>
     </footer>
     <!-- Bootstrap JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
