dd9fe3ea7beea6549232eb20a13dd31a593edef5
AndreHLudwig <andre.udesc@hotmail.com>
2024-04-15 13:47:53 -0300

    Final project movies

--- 19_MOVIESTAR/newmovie.php
+++ 19_MOVIESTAR/newmovie.php
@@ -1,17 +1,17 @@
 <?php
-    require_once("templates/header.php");
+  require_once("templates/header.php");
 
-    //Verifica se usuário está autenticado
-    require_once("models/User.php");
-    require_once("dao/UserDAO.php");
+  // Verifica se usuário está autenticado
+  require_once("models/User.php");
+  require_once("dao/UserDAO.php");
 
-    $user = new User();
-    $userDao = new UserDao($conn, $BASE_URL);
+  $user = new User();
+  $userDao = new UserDao($conn, $BASE_URL);
 
-    $userData = $userDao->verifyToken(true);
+  $userData = $userDao->verifyToken(true);
+
 ?>
-
-<div id="main-container" class="container-fluid">
+  <div id="main-container" class="container-fluid">
     <div class="offset-md-4 col-md-4 new-movie-container">
       <h1 class="page-title">Adicionar Filme</h1>
       <p class="page-description">Adicione sua crítica e compartilhe com o mundo!</p>
@@ -55,4 +55,3 @@
 <?php
   require_once("templates/footer.php");
 ?>
-    
